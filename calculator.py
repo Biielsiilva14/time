78caa97e3a83cc55286943bd38d7aa29cb772cea
Biielsiilva14 <Zasrana@protonmail.com>
2025-06-27 19:46:13 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,7 +20,7 @@ def main():
             print(f"Your BMI is: {BMI}")
             if bmi < 18.5:
                 print("You are underweight.")
-            elif 18.5 <= bmi < 24.9:
+            elif 18.5 <= bmi < 24.5:
                 print("You have a normal weight.")
             elif 25 <= bmi < 29.9:
                 print("You are overweight.")
